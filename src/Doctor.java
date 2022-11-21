dcf4d87f7ffff12ae1042c6275267100f08d60d2
Gustavo Galvis <ggalvis216@gmail.com>
2022-11-20 21:24:56 -0500

    Constructor

--- src/Doctor.java
+++ src/Doctor.java
@@ -3,6 +3,15 @@ public class Doctor {
     String name;
     String specialty;
 
+    Doctor(){
+        System.out.println("Construyendo el Objeto Doctor");
+    }
+
+    Doctor(String name) {
+        System.out.println("El nombre del Doctor asignado es: " + name);
+        this.name = name;
+    }
+
     public void showName() {
         System.out.println(name);
     }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Andres Salgado");
+        myDoctor.showName();
         myDoctor.name = "Alejandro Rodríguez";
         myDoctor.showName();
 
